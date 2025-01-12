7150c167649f7f3635e4b761feb1dfe4004d4661
xieby1 <xieby1@outlook.com>
2025-01-12 14:21:06 +0800

    add gitea

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
+all: result-gitea result-github
+
 result-github: ./example-github.nix
 	nix-build $< --argstr github_token github_pat_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -o $@
+
+result-gitea: ./example-gitea.nix
+	nix-build $< -o $@
---
+++ example-gitea.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {}
+, instance
+, token
+}: pkgs.callPackage ./. {
+  runner = pkgs.gitea-actions-runner;
+  runner_sh = let
+    registerCmd = [
+      "act_runner register"
+      "--no-interactive"
+      "--instance '${instance}'"
+      "--token ${token}"
+      "--name $HOSTNAME-$(TZ=UTC-8 date +%y%m%d%H%M%S)"
+      "--labels 'self-hosted,Linux,X64,nix'"
+      "$@"
+    ];
+  in builtins.toFile "runner.sh" ''
+    echo ${toString registerCmd}
+    ${toString registerCmd}
+    act_runner daemon
+  '';
+  extraPodmanOpts = [];
+  extraPkgsInPATH = [pkgs.git];
+}
