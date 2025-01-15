bcae4282d9a822b1c9c82df76c3131f9d163a8f7
xieby1 <xieby1@outlook.com>
2025-01-15 10:31:10 +0800

    example-gitea: add timeout comments

--- example-gitea.nix
+++ example-gitea.nix
@@ -2,6 +2,10 @@
 , instance
 , token
 }: let
+  # https://github.com/go-gitea/gitea/issues/30161
+  # There are two places where we need to configure the runner timeout:
+  # 1. Gitea instance configure: custom/conf/app.ini: ENDLESS_TASK_TIMEOUT
+  # 2. Runner instance configure: the yaml below: timeout
   config_yml = pkgs.runCommand "gitea-runner-config.yml" {} ''
     ${pkgs.gitea-actions-runner}/bin/act_runner generate-config > $out
     sed -i 's/\<timeout:.*$/timeout: 72h/' $out
